76d3eed5135a70cb71726f74612bc506694fe9a6
MinGRn <MinGRn97@gmail.com>
2019-01-18 16:56:15 +0800

    修改 application 工具类;

--- src/main/java/com/mingrn/keeper/utils/context/SpringContextUtil.java
+++ src/main/java/com/mingrn/keeper/utils/context/SpringContextUtil.java
@@ -1,6 +1,7 @@
 package com.mingrn.keeper.utils.context;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
@@ -29,16 +30,19 @@ public class SpringContextUtil implements ApplicationContextAware {
 		SpringContextUtil.applicationContext = applicationContext;
 	}
 
+	/**
+	 * 获取 {@link ApplicationContext}
+	 */
 	public static ApplicationContext getApplicationContext() {
 		checkApplicationContext();
 		return applicationContext;
 	}
 
 	/**
-	 * 获取bean
+	 * 获取 bean
 	 *
 	 * @param name bean 名称
-	 * @return T
+	 * @return {@link Object}
 	 */
 	public static Object getBean(String name) {
 		checkApplicationContext();
@@ -69,11 +73,58 @@ public class SpringContextUtil implements ApplicationContextAware {
 	}
 
 	/**
+	 * BeanFactory是否包含指定名称bean定义
+	 *
+	 * @param name bean 名称
+	 * @return {@link Boolean}
+	 */
+	public static boolean containsBean(String name) {
+		checkApplicationContext();
+		return applicationContext.containsBean(name);
+	}
+
+	/**
+	 * 判断以给定名字注册的bean定义是一个singleton还是一个prototype。
+	 * 如果与给定名字相应的bean定义没有被找到，将会抛出 {@link NoSuchBeanDefinitionException} 异常
+	 *
+	 * @param name bean 名称
+	 * @return {@link Boolean}
+	 * @throws org.springframework.beans.factory.NoSuchBeanDefinitionException
+	 */
+	public static boolean isSingleton(String name) throws NoSuchBeanDefinitionException {
+		checkApplicationContext();
+		return applicationContext.isSingleton(name);
+	}
+
+	/**
+	 * 获取 bean 类型
+	 *
+	 * @param name bean 名称
+	 * @return Class 注册对象的类型
+	 * @throws org.springframework.beans.factory.NoSuchBeanDefinitionException
+	 */
+	public static Class<?> getType(String name) throws NoSuchBeanDefinitionException {
+		checkApplicationContext();
+		return applicationContext.getType(name);
+	}
+
+	/**
+	 * 获取指定 bean名字在bean定义中的别名
+	 *
+	 * @param name bean 名称
+	 * @return {@link String...}
+	 * @throws org.springframework.beans.factory.NoSuchBeanDefinitionException
+	 */
+	public static String[] getAliases(String name) throws NoSuchBeanDefinitionException {
+		return applicationContext.getAliases(name);
+	}
+
+	/**
 	 * 检查 applicationContext
 	 */
 	private static void checkApplicationContext() {
 		if (applicationContext == null) {
-			throw new IllegalStateException("ApplicationContext not injected please define the SpringContextUtil use annotations or spring.xml");
+			throw new IllegalStateException("ApplicationContext not injected please define the SpringContextUtil use annotations: `@Component` or spring.xml");
 		}
 	}
 }
